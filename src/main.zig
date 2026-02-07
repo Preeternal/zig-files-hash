97c1dbcc363dcce010569113fb2790523c32813d
Preeternal <preeternal@gmail.com>
2026-02-07 22:43:27 +0200

    refactor: unify hash calculation and extend tests across algorithms

--- src/main.zig
+++ src/main.zig
@@ -18,75 +18,40 @@ pub fn main() !void {
     };
     std.debug.print("First argument: {s}\n", .{path});
 
-    const xxh3_64 = try fileHash(Xxh3_64, path, null);
-    std.debug.print("Xxh3_64 = {x}\n", .{xxh3_64});
-
-    const blake3 = try fileHash(Blake3, path, null);
-    std.debug.print("Blake3 = {s}\n", .{toHex(blake3)[0..]});
-
-    const blake3_keyed = try fileHash(Blake3, path, .{
-        .key = "my_secret_key_123456789012122222", // must be 32 bytes for Blake3
-    });
-    std.debug.print("Blake3 keyed = {s}\n", .{toHex(blake3_keyed)[0..]});
-
-    const blake3_from_str = try stringHash(Blake3, "Hello, world!", null);
-    std.debug.print("Blake3 from string = {s}\n", .{toHex(blake3_from_str)[0..]});
-
-    const sha256T192 = try fileHash(Sha256T192, path, null);
-    const sha256T192_hex = std.fmt.bytesToHex(sha256T192, .lower);
-    std.debug.print("SHA-256/192 = {s}\n", .{sha256T192_hex[0..]});
-
-    const sha224 = try fileHash(Sha224, path, null);
-    const sha224_hex = std.fmt.bytesToHex(sha224, .lower);
-    std.debug.print("SHA-224 = {s}\n", .{sha224_hex[0..]});
-
-    const sha256 = try fileHash(Sha256, path, null);
-    const sha256_hex = std.fmt.bytesToHex(sha256, .lower);
-    std.debug.print("SHA-256 = {s}\n", .{sha256_hex[0..]});
-
-    const hmacSha224 = try fileHash(HmacSha224, path, .{
-        .key = "my_secret_key",
-    });
-    const hmacSha224_hex = std.fmt.bytesToHex(hmacSha224, .lower);
-    std.debug.print("HMAC-SHA-224 = {s}\n", .{hmacSha224_hex[0..]});
-
-    const hmacSha256 = try fileHash(HmacSha256, path, .{
-        .key = "my_secret_key",
-    });
-    const hmacSha256_hex = std.fmt.bytesToHex(hmacSha256, .lower);
-    std.debug.print("HMAC-SHA-256 = {s}\n", .{hmacSha256_hex[0..]});
-
-    const hmacSha384 = try fileHash(HmacSha384, path, .{
-        .key = "my_secret_key",
-    });
-    const hmacSha384_hex = std.fmt.bytesToHex(hmacSha384, .lower);
-    std.debug.print("HMAC-SHA-384 = {s}\n", .{hmacSha384_hex[0..]});
-
-    const hmacSha512 = try fileHash(HmacSha512, path, .{
-        .key = "my_secret_key",
-    });
-    const hmacSha512_hex = std.fmt.bytesToHex(hmacSha512, .lower);
-    std.debug.print("HMAC-SHA-512 = {s}\n", .{hmacSha512_hex[0..]});
-
-    const hmacMd5 = try fileHash(HmacMd5, path, .{
-        .key = "my_secret_key",
-    });
-    const hmacMd5_hex = std.fmt.bytesToHex(hmacMd5, .lower);
-    std.debug.print("HMAC-MD5 = {s}\n", .{hmacMd5_hex[0..]});
-
-    const hmacSha1 = try fileHash(HmacSha1, path, .{
-        .key = "my_secret_key",
-    });
-    const hmacSha1_hex = std.fmt.bytesToHex(hmacSha1, .lower);
-    std.debug.print("HMAC-SHA-1 = {s}\n", .{hmacSha1_hex[0..]});
-
-    const md5 = try fileHash(MD5, path, null);
-    const md5_hex = std.fmt.bytesToHex(md5, .lower);
-    std.debug.print("MD5 = {s}\n", .{md5_hex[0..]});
+    try calculateHashForEverything(path);
 }
 
 fn toHex(input: anytype) [input.len * 2]u8 {
     return std.fmt.bytesToHex(input, .lower); // или .upper
+}
+
+fn getAlgorithmName(comptime H: type) []const u8 {
+    if (@hasDecl(H, "name")) return H.name;
+    return @typeName(H);
+}
+
+fn calculateHashForEverything(path: [:0]const u8) !void {
+    inline for (Algorithms) |H| {
+        if (H == Blake3) {
+            // 1) unkeyed
+            const h1 = try fileHash(Blake3, path, null);
+            std.debug.print("BLAKE3 = {s}\n", .{toHex(h1)[0..]});
+
+            // 2) keyed
+            const opts = HashOptions{ .key = "my_secret_key_123456789012122222" };
+            const h2 = try fileHash(Blake3, path, opts);
+            std.debug.print("BLAKE3-KEYED = {s}\n", .{toHex(h2)[0..]});
+
+            continue;
+        } else {
+            const options = getOptionsForTests(H, null);
+            const h = try fileHash(H, path, options);
+            switch (H) {
+                Xxh3_64 => std.debug.print("{s} = {x}\n", .{ getAlgorithmName(H), h }),
+                else => std.debug.print("{s} = {s}\n", .{ getAlgorithmName(H), toHex(h)[0..] }),
+            }
+        }
+    }
 }
 
 // export type THashAlgorithm =
@@ -103,6 +68,21 @@ fn toHex(input: anytype) [input.len * 2]u8 {
 
 // const HashMode = enum { hash, hmac, keyed };
 
+const Algorithms = .{
+    Sha256T192, //
+    Sha224,
+    Sha256,
+    MD5,
+    Xxh3_64,
+    Blake3,
+    HmacSha224,
+    HmacSha256,
+    HmacSha384,
+    HmacSha512,
+    HmacMd5,
+    HmacSha1,
+};
+
 const HashOptions = struct {
     // mode: HashMode = .hash,
     seed: ?u64 = null,
@@ -112,8 +92,8 @@ const HashOptions = struct {
     // key_encoding: ?KeyEncoding = null,
 };
 
-fn fileHash(comptime H: type, path: []const u8, options: ?HashOptions) !H.Digest {
-    var file = try std.fs.cwd().openFile(path, .{});
+pub fn fileHashInDir(comptime H: type, dir: std.fs.Dir, sub_path: []const u8, options: ?HashOptions) !H.Digest {
+    var file = try dir.openFile(sub_path, .{});
     defer file.close();
     var buf: [64 * 1024]u8 = undefined;
     var hasher = try H.init(options);
@@ -129,6 +109,10 @@ fn fileHash(comptime H: type, path: []const u8, options: ?HashOptions) !H.Digest
     }
 }
 
+fn fileHash(comptime H: type, path: []const u8, options: ?HashOptions) !H.Digest {
+    return fileHashInDir(H, std.fs.cwd(), path, options);
+}
+
 fn stringHash(comptime H: type, data: []const u8, options: ?HashOptions) !H.Digest {
     var hasher = try H.init(options);
     hasher.update(data);
@@ -138,6 +122,13 @@ fn stringHash(comptime H: type, data: []const u8, options: ?HashOptions) !H.Dige
 fn Sha2_32(comptime Bits: u16) type {
     return struct {
         const Self = @This();
+
+        pub const name = switch (Bits) {
+            192 => "SHA-256/192",
+            224 => "SHA-224",
+            256 => "SHA-256",
+            else => unreachable,
+        };
 
         const Inner = switch (Bits) {
             192 => std.crypto.hash.sha2.Sha256T192,
@@ -177,6 +168,16 @@ fn Hmac(comptime H: type) type {
     return struct {
         const Self = @This();
 
+        pub const name = switch (H) {
+            std.crypto.auth.hmac.sha2.HmacSha224 => "HMAC-SHA-224",
+            std.crypto.auth.hmac.sha2.HmacSha256 => "HMAC-SHA-256",
+            std.crypto.auth.hmac.sha2.HmacSha384 => "HMAC-SHA-384",
+            std.crypto.auth.hmac.sha2.HmacSha512 => "HMAC-SHA-512",
+            std.crypto.auth.hmac.HmacMd5 => "HMAC-MD5",
+            std.crypto.auth.hmac.HmacSha1 => "HMAC-SHA-1",
+            else => unreachable,
+        };
+
         const Inner = H;
 
         inner: Inner,
@@ -212,6 +213,8 @@ const HmacMd5 = Hmac(std.crypto.auth.hmac.HmacMd5);
 const HmacSha1 = Hmac(std.crypto.auth.hmac.HmacSha1);
 
 const MD5 = struct {
+    pub const name = "MD5";
+
     inner: std.crypto.hash.Md5,
 
     pub fn init(options: ?HashOptions) !MD5 {
@@ -233,6 +236,8 @@ const MD5 = struct {
 };
 
 const Xxh3_64 = struct {
+    pub const name = "XXH3-64";
+
     inner: std.hash.XxHash3,
 
     pub fn init(options: ?HashOptions) !Xxh3_64 {
@@ -254,10 +259,13 @@ const Xxh3_64 = struct {
 const Blake3 = struct {
     const Self = @This();
 
+    pub const name = "BLAKE3";
+
     inner: std.crypto.hash.Blake3,
 
     pub fn init(options: ?HashOptions) !Self {
         var opt: std.crypto.hash.Blake3.Options = .{};
+        opt.key = null; // not keyed by default
         if (options) |o| {
             if (o.key) |k| {
                 if (k.len != 32) {
@@ -266,11 +274,7 @@ const Blake3 = struct {
                 var tmp: [32]u8 = undefined;
                 std.mem.copyForwards(u8, tmp[0..], k[0..32]);
                 opt.key = tmp;
-            } else {
-                opt.key = null; // not keyed
             }
-        } else {
-            opt.key = null; // not keyed
         }
 
         return .{ .inner = std.crypto.hash.Blake3.init(opt) };
@@ -289,11 +293,69 @@ const Blake3 = struct {
     }
 };
 
-test "deterministic string hash" {
-    const hash1 = try stringHash(Blake3, "Hello, world!", null);
-    const hash2 = try stringHash(Blake3, "Hello, world!", null);
+fn getOptionsForTests(comptime H: type, is_keyed_blake3: ?bool) ?HashOptions {
+    if (is_keyed_blake3) |keyed| {
+        if (keyed and H == Blake3) {
+            return HashOptions{ .key = "my_secret_key_123456789012122222" };
+        }
+    }
+    return switch (H) {
+        HmacSha224, HmacSha256, HmacSha384, HmacSha512, HmacMd5, HmacSha1 => .{
+            .key = "my_secret_key",
+        },
+        else => null,
+    };
+}
+
+fn expectDeterministicStringHash(comptime H: type, is_keyed_blake3: ?bool) !void {
+    const options = getOptionsForTests(H, is_keyed_blake3);
+    const data = "Hello, world!";
+    const hash1 = try stringHash(H, data, options);
+    const hash2 = try stringHash(H, data, options);
     try std.testing.expectEqual(hash1, hash2);
-    std.debug.print("✓ Deterministic string hash Test passed!\n", .{});
+}
+
+test "deterministic string hash" {
+    inline for (Algorithms) |H| {
+        try expectDeterministicStringHash(H, null);
+        if (H == Blake3) {
+            try expectDeterministicStringHash(H, true);
+        }
+    }
+}
+
+fn expectFileHashDeterminismAndConsistency(comptime H: type, is_keyed_blake3: ?bool) !void {
+    var tmp = std.testing.tmpDir(.{});
+    defer tmp.cleanup();
+    const sub_path = "test.bin";
+    const data = "Hello, world!";
+
+    {
+        const file = try tmp.dir.createFile(sub_path, .{
+            .truncate = true,
+        });
+        defer file.close();
+        try file.writeAll(data);
+    }
+
+    const options = getOptionsForTests(H, is_keyed_blake3);
+
+    const hash1 = try fileHashInDir(H, tmp.dir, sub_path, options);
+    const hash2 = try fileHashInDir(H, tmp.dir, sub_path, options);
+
+    try std.testing.expectEqual(hash1, hash2);
+
+    const hash3 = try stringHash(H, data, options);
+    try std.testing.expectEqual(hash1, hash3);
+}
+
+test "file hash determinism and consistency with string hash" {
+    inline for (Algorithms) |H| {
+        try expectFileHashDeterminismAndConsistency(H, null);
+        if (H == Blake3) {
+            try expectFileHashDeterminismAndConsistency(H, true);
+        }
+    }
 }
 
 // test "simple test" {
